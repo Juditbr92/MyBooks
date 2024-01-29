7d7635bd39a425ed09b962df942efb77b2451f0c
Juditbr92 <juditbardonromero@gmail.com>
2024-01-29 18:17:54 +0100

    formulario model driven

--- src/app/component/form-register/form-register.component.ts
+++ src/app/component/form-register/form-register.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-form-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-register.component.html',
   styleUrl: './form-register.component.css'
 })
-export class FormRegisterComponent {
 
+export class FormRegisterComponent {
+  public registerForm: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required), 
+    surname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.email, Validators.required]), 
+    password: new FormControl('', [Validators.required, Validators.minLength(5)])
+  });
+
+  public handleSubmit(): void{
+    console.log(this.registerForm.value)
+    console.log(this.registerForm)
+    console.log('Errores de contraseña:', this.registerForm.get('password')?.errors);
+  }
+
+  
 }

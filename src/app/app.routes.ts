4f3b1498dd47ac6910fa9f3ede52c7596ce55563
Juditbr92 <juditbardonromero@gmail.com>
2024-01-29 18:36:10 +0100

    commit de los ultimos cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { BookComponent } from './pages/book/book.component';
 import { AddBookComponent } from './pages/add-book/add-book.component';
 import { UpdateBookComponent } from './pages/update-book/update-book.component';
+import { LoginComponent } from './component/login/login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/homepage', pathMatch: 'full' },
@@ -13,5 +14,6 @@ export const routes: Routes = [
     { path: 'profile', component: ProfileComponent},
     { path: 'books', component: BookComponent},
     { path: 'addBooks', component: AddBookComponent},
-    { path: 'updateBooks', component: UpdateBookComponent}
+    { path: 'updateBooks', component: UpdateBookComponent}, 
+    { path: 'log-in', component: LoginComponent}
 ];
